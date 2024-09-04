18f1083e10bf8464cf93bb1a94909fa7bd4db550
Chang-Ju (David) Kim <cjoak1028@gmail.com>
2024-09-03 21:11:48 -0400

    Refactored header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,30 +8,30 @@ import avatar from "../assets/image-avatar.png";
 const Header = () => {
   return (
     <header className="p-8 h-screen min-h-[600px]">
-      <div className="h-full w-24 bg-container rounded-[20px] flex flex-col items-center justify-between py-8">
-        <div>
-          <img src={logo} className="w-8 mb-[75px]" />
-          <nav>
-            <ul className="flex flex-col items-center gap-10">
-              <li>
-                <img src={homeIcon} />
-              </li>
-              <li>
-                <img src={moviesIcon} />
-              </li>
-              <li>
-                <img src={tvIcon} />
-              </li>
-              <li>
-                <img src={bookmarkIcon} />
-              </li>
-            </ul>
-          </nav>
+      <div className="h-full w-24 bg-container rounded-[20px] flex flex-col items-center py-8 gap-[4.7rem]">
+        <img src={logo} className="w-8" />
+        <nav>
+          <ul className="flex flex-col items-center gap-10">
+            <li>
+              <img src={homeIcon} />
+            </li>
+            <li>
+              <img src={moviesIcon} />
+            </li>
+            <li>
+              <img src={tvIcon} />
+            </li>
+            <li>
+              <img src={bookmarkIcon} />
+            </li>
+          </ul>
+        </nav>
+        <div className="flex-1 flex items-end">
+          <img
+            src={avatar}
+            className="rounded-full w-10 h-10 border-solid border border-on-container"
+          />
         </div>
-        <img
-          src={avatar}
-          className="rounded-full w-10 h-10 border-solid border border-on-container"
-        />
       </div>
     </header>
   );
