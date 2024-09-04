56ad4a1e4cbfeee1e7df66f6624f46058c41c6ec
Chang-Ju (David) Kim <cjoak1028@gmail.com>
2024-09-03 22:13:37 -0400

    Made header responsive to all screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="flex flex-row bg-surface h-screen">
+    <div className="flex flex-row bg-surface h-screen md:flex-col">
       <Header />
       <main className="flex-1 py-8 overflow-auto custom-scroll-bar">
         <h1>main section</h1>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,29 +7,29 @@ import avatar from "../assets/image-avatar.png";
 
 const Header = () => {
   return (
-    <header className="p-8 h-screen min-h-[600px]">
-      <div className="h-full w-24 bg-container rounded-[20px] flex flex-col items-center py-8 gap-[4.7rem]">
-        <img src={logo} className="w-8" />
+    <header className="p-8 h-screen min-h-[37.5rem] md:p-6 md:h-fit md:min-h-0 sm:p-0">
+      <div className="h-full w-24 bg-container rounded-[20px] flex flex-col items-center py-8 gap-[4.7rem] md:flex-row md:h-fit md:w-full md:p-5 md:justify-between md:gap-0 sm:p-4">
+        <img src={logo} className="w-8 sm:w-6" />
         <nav>
-          <ul className="flex flex-col items-center gap-10">
+          <ul className="flex flex-col items-center gap-10 md:flex-row md:gap-8 sm:gap-6">
             <li>
-              <img src={homeIcon} />
+              <img src={homeIcon} className="h-5 sm:h-4" />
             </li>
             <li>
-              <img src={moviesIcon} />
+              <img src={moviesIcon} className="h-5 sm:h-4" />
             </li>
             <li>
-              <img src={tvIcon} />
+              <img src={tvIcon} className="h-5 sm:h-4" />
             </li>
             <li>
-              <img src={bookmarkIcon} />
+              <img src={bookmarkIcon} className="h-5 sm:h-4" />
             </li>
           </ul>
         </nav>
-        <div className="flex-1 flex items-end">
+        <div className="flex-1 flex items-end md:flex-none">
           <img
             src={avatar}
-            className="rounded-full w-10 h-10 border-solid border border-on-container"
+            className="rounded-full w-10 h-10 border-solid border border-on-container md:w-8 md:h-8 sm:w-6 sm:h-6"
           />
         </div>
       </div>
